5bd629df8adf8e75891e6f99ef66105bf56fb141
Tang Yaguang <texnician@gmail.com>
2011-03-06 14:11:45 +0000

    Exercise 1.3

--- ch1-1-6.lisp
+++ ch1-1-6.lisp
@@ -31,3 +31,37 @@
          ((> *b* *a*) *b*)
          (t -1))
    (+ *a* 1))
+
+;; Exercise 1.2
+
+(/ (+ 5 4 (- 2 (- 3 (+ 6 4/5))))
+   (* 3 (- 6 2) (- 2 7)))
+
+;; Exercise 1.3
+
+(defun square (x)
+  (* x x))
+
+(defun max1 (x y)
+  (if (> x y)
+      x
+      y))
+
+(defun min1 (x y)
+  (if (< x y)
+      x
+      y))
+
+(defun sum-of-square (x y)
+  (+ (square x) (square y)))
+
+(defun sum-of-two-larger-square (x y z)
+  (cond ((< x (min1 y z)) (sum-of-square y z))
+        (t (sum-of-square x (max1 y z)))))
+
+(sum-of-two-larger-square 1 2 3)
+(sum-of-two-larger-square 1 3 2)
+(sum-of-two-larger-square 2 1 3)
+(sum-of-two-larger-square 2 3 1)
+(sum-of-two-larger-square 3 1 2)
+(sum-of-two-larger-square 3 2 1)
