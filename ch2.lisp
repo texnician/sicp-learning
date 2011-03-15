dff788d833946aa5ac32c0d2b9af6c28b5762f07
Tang Yaguang <texnician@gmail.com>
2011-03-15 12:51:46 +0000

    l2p!

--- ch2.lisp
+++ ch2.lisp
@@ -881,3 +881,34 @@
         (append rest (map 'list #'(lambda (x)
                                     (cons (car s) x))
                           rest)))))
+
+;; *Exercise 2.33:* Fill in the missing expressions to complete the
+;; following definitions of some basic list-manipulation operations
+;; as accumulations:
+
+;;      (define (map p sequence)
+;;        (accumulate (lambda (x y) <??>) nil sequence))
+
+;;      (define (append seq1 seq2)
+;;        (accumulate cons <??> <??>))
+
+;;      (define (length sequence)
+;;        (accumulate <??> 0 sequence))
+(defun sicp-accumulate (op initial sequence)
+  (if (null sequence)
+      initial
+      (funcall op (car sequence) (sicp-accumulate op initial (cdr sequence)))))
+
+(defun sicp-map (p sequence)
+  (sicp-accumulate #'(lambda (x y) (cons (funcall p x) y)) nil sequence))
+
+(sicp-map #'(lambda (x)
+              (* x x)) '(1 2 3 4 5))
+
+(defun sicp-append (seq1 seq2)
+  (sicp-accumulate #'cons seq2 seq1))
+
+(defun sicp-length (sequence)
+  (sicp-accumulate #'(lambda (x y) (+ 1 y)) 0 sequence))
+
+; (sicp-length '(1 2 3 4 5 6 7))
