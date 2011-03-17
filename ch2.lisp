6bc0d8c0433f21e0de59bfb75ac1f3c7dc0bd5af
Tang Yaguang <texnician@gmail.com>
2011-03-17 10:05:34 +0000

    *** empty log message ***

--- ch2.lisp
+++ ch2.lisp
@@ -1296,4 +1296,24 @@
                        (cdr q)))))
            (iter t rest-queens)))
 
-;(length (queens 8))
+;(time (queens 8))
+
+;; Exercise 2.43.  Louis Reasoner is having a terrible time doing exercise
+;; 2.42. His queens procedure seems to work, but it runs extremely
+;; slowly. (Louis never does manage to wait long enough for it to solve even the
+;; 6¡Á 6 case.) When Louis asks Eva Lu Ator for help, she points out that he has
+;; interchanged the order of the nested mappings in the flatmap, writing it as
+
+;; (flatmap
+;;  (lambda (new-row)
+;;    (map (lambda (rest-of-queens)
+;;           (adjoin-position new-row k rest-of-queens))
+;;         (queen-cols (- k 1))))
+;;  (enumerate-interval 1 board-size))
+
+;; Explain why this interchange makes the program run slowly. Estimate how long
+;; it will take Louis's program to solve the eight-queens puzzle, assuming that
+;; the program in exercise 2.42 solves the puzzle in time T.
+
+;; louis's program compute each `queen-cols' N times. thus the N step recursive
+;; call will be N^N times slower than T.
