1e8859867e78c8d0932b98bb1faa206dc35b4f0c
Tang Yaguang <texnician@gmail.com>
2011-03-23 15:59:26 +0000

    huffman tree

--- ch2.lisp
+++ ch2.lisp
@@ -2029,3 +2029,211 @@
 
 ;(tree->list-1 (union-binary-tree-set *tree1* *tree2*))
 ;(tree->list-2 (intersection-binary-tree-set *tree1* *tree2*))
+
+(defun make-huffman-leaf (symbol weight)
+  (list 'leaf symbol weight))
+
+(defun huffman-leafp (object)
+  (eq (car object) 'leaf))
+
+(defun symbol-huffman-leaf (x)
+  (cadr x))
+
+(defun weight-huffman-leaf (x)
+  (caddr x))
+
+(defun make-code-tree (left right)
+  (list left right
+        (append (huffman-symbols left) (huffman-symbols right))
+        (+ (huffman-weight left) (huffman-weight right))))
+
+(defun code-tree-left-branch (tree)
+  (car tree))
+
+(defun code-tree-right-branch (tree)
+  (cadr tree))
+
+(defun huffman-symbols (tree)
+  (if (huffman-leafp tree)
+      (list (symbol-huffman-leaf tree))
+      (caddr tree)))
+
+(defun huffman-weight (tree)
+  (if (huffman-leafp tree)
+      (weight-huffman-leaf tree)
+      (cadddr tree)))
+
+(defun huffman-decode (bits tree)
+  (labels ((decode-1 (bits current-branch)
+             (if (null bits)
+                 nil
+                 (let ((next-branch (select-huffman-branch (car bits) current-branch)))
+                   (if (huffman-leafp next-branch)
+                       (cons (symbol-huffman-leaf next-branch)
+                             (decode-1 (cdr bits) tree))
+                       (decode-1 (cdr bits) next-branch))))))
+    (decode-1 bits tree)))
+
+(defun select-huffman-branch (bit tree)
+  (cond ((= 0 bit) (code-tree-left-branch tree))
+        ((= 1 bit) (code-tree-right-branch tree))
+        (t (error "bat bits -- CHOOSE BRANCH ~a" bit))))
+
+(defun adjoin-huffman-set (x set)
+  (cond ((null set) (list x))
+        ((< (huffman-weight x) (huffman-weight (car set)))
+         (cons x set))
+        (t (cons (car set) (adjoin-huffman-set x (cdr set))))))
+
+(defun make-huffman-leaf-set (pairs)
+  (if (null pairs)
+      nil
+      (let ((pair (car pairs)))
+        (adjoin-huffman-set (make-huffman-leaf (car pair) (cadr pair))
+                            (make-huffman-leaf-set (cdr pairs))))))
+
+;; *Exercise 2.67:* Define an encoding tree and a sample message:
+
+;;      (define sample-tree
+;;        (make-code-tree (make-leaf 'A 4)
+;;                        (make-code-tree
+;;                         (make-leaf 'B 2)
+;;                         (make-code-tree (make-leaf 'D 1)
+;;                                         (make-leaf 'C 1)))))
+
+;;      (define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+;; Use the `decode' procedure to decode the message, and give the result.
+(defparameter *sample-huffman-tree*
+    (make-code-tree (make-huffman-leaf 'A 4)
+                    (make-code-tree
+                     (make-huffman-leaf 'B 2)
+                     (make-code-tree (make-huffman-leaf 'D 1)
+                                     (make-huffman-leaf 'C 1)))))
+
+(defparameter *sample-huffman-message* '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+;(huffman-decode *sample-huffman-message* *sample-huffman-tree*)
+
+;; *Exercise 2.68:* The `encode' procedure takes as arguments a message and a
+;; tree and produces the list of bits that gives the encoded message.
+
+;;      (define (encode message tree)
+;;        (if (null? message)
+;;            '()
+;;            (append (encode-symbol (car message) tree)
+;;                    (encode (cdr message) tree))))
+
+;; `Encode-symbol' is a procedure, which you must write, that returns the list
+;; of bits that encodes a given symbol according to a given tree.  You should
+;; design `encode-symbol' so that it signals an error if the symbol is not in
+;; the tree at all.  Test your procedure by encoding the result you obtained in
+;; *note Exercise 2-67:: with the sample tree and seeing whether it is the same
+;; as the original sample message.
+(defun huffman-encode (msg tree)
+  (if (null msg)
+      nil
+      (append (huffman-encode-symbol (car msg) tree)
+              (huffman-encode (cdr msg) tree))))
+
+(defun huffman-encode-symbol (symbol tree)
+  (cond ((null tree)
+         (error "NULL HUFFMAN TREE"))
+        ((huffman-leafp tree)
+         (if (eq symbol (symbol-huffman-leaf tree))
+             nil
+             (error "-- INVALID SYMBOL ~a" symbol)))
+        (t (if (member symbol (huffman-symbols (code-tree-left-branch tree)))
+               (cons 0 (huffman-encode-symbol symbol (code-tree-left-branch tree)))
+               (cons 1 (huffman-encode-symbol symbol (code-tree-right-branch tree)))))))
+
+;(huffman-encode '(A D A B B C A) *sample-huffman-tree*)
+
+;; *Exercise 2.69:* The following procedure takes as its argument a list of
+;; symbol-frequency pairs (where no symbol appears in more than one pair) and
+;; generates a Huffman encoding tree according to the Huffman algorithm.
+
+;;      (define (generate-huffman-tree pairs)
+;;        (successive-merge (make-leaf-set pairs)))
+
+;; `Make-leaf-set' is the procedure given above that transforms the list of
+;; pairs into an ordered set of leaves.  `Successive-merge' is the procedure you
+;; must write, using `make-code-tree' to successively merge the smallest-weight
+;; elements of the set until there is only one element left, which is the
+;; desired Huffman tree.  (This procedure is slightly tricky, but not really
+;; complicated.  If you find yourself designing a complex procedure, then you
+;; are almost certainly doing something wrong.  You can take significant
+;; advantage of the fact that we are using an ordered set representation.)
+(defun generate-huffman-tree (pairs)
+  (successive-merge (make-huffman-leaf-set pairs)))
+
+(defun successive-merge (leaf-sets)
+  (let ((leaf1 (car leaf-sets))
+        (leaf2 (cadr leaf-sets)))
+    (if (or (null leaf1) (null leaf2))
+        (car leaf-sets)
+        (successive-merge (adjoin-huffman-set (make-code-tree leaf1 leaf2)
+                                              (cddr leaf-sets))))))
+
+;(defparameter *leaf-pairs* '((C 8) (D 6) (E 15) (F 2) (G 5) (A 7) (B 1)))
+
+;; *Exercise 2.70:* The following eight-symbol alphabet with associated relative
+;; frequencies was designed to efficiently encode the lyrics of 1950s rock
+;; songs.  (Note that the "symbols" of an "alphabet" need not be individual
+;; letters.)
+
+;;      A     2 NA   16
+;;      BOOM  1 SHA  3
+;;      GET   2 YIP  9
+;;      JOB   2 WAH  1
+
+;; Use `generate-huffman-tree' (*note Exercise 2-69::) to generate a
+;; corresponding Huffman tree, and use `encode' (*note Exercise 2-68::) to
+;; encode the following message:
+
+;;      Get a job
+
+;;      Sha na na na na na na na na
+
+;;      Get a job
+
+;;      Sha na na na na na na na na
+
+;;      Wah yip yip yip yip yip yip yip yip yip
+
+;;      Sha boom
+
+;; How many bits are required for the encoding?  What is the smallest number of
+;; bits that would be needed to encode this song if we used a fixed-length code
+;; for the eight-symbol alphabet?
+
+(defparameter *lynrics-of-rock* '((A 2) (NA 16) (BOOM 1) (SHA  3)
+                                  (GET 2) (YIP 9) (JOB 2) (WAH  1)))
+
+(generate-huffman-tree *lynrics-of-rock*)
+
+(huffman-encode '(Get a job                           ; (* 2 7) => 14
+                  Sha na na na na na na na na         ; (* 2 (+ 16 3)) => 38
+                  Get a job
+                  Sha na na na na na na na na
+                  Wah yip yip yip yip yip yip yip yip yip ; 27
+                  Sha boom)                               ; 7
+                (generate-huffman-tree *lynrics-of-rock*))
+;; (/ 84 (* 8 (+ 14 38 27 7))) 21/172
+
+;; *Exercise 2.71:* Suppose we have a Huffman tree for an alphabet of n symbols,
+;; and that the relative frequencies of the symbols are 1, 2, 4, ..., 2^(n-1).
+;; Sketch the tree for n=5 for n=10.  In such a tree (for general n) how may
+;; bits are required to encode the most frequent symbol?  the least frequent
+;; symbol?
+(defun test-huffman-freq (n)
+  (let ((tree
+         (generate-huffman-tree (accumulate #'cons nil #'(lambda (x)
+                                                           (let ((e (expt 2 x)))
+                                                             (list x e)))
+                                            0 #'1+ (1- n)))))
+    (list (length (huffman-encode (list (1- n)) tree))
+          (length (huffman-encode '(0) tree)))))
+
+; (test-huffman-freq 5) => (1 4)
+; (test-huffman-freq 10) => (1 9)
