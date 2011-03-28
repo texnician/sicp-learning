0e2f7ca49001f728527465dd32e44a88cf4a7045
Tang Yaguang <texnician@gmail.com>
2011-03-28 15:49:08 +0000

    mote-carlo

--- ch3.lisp
+++ ch3.lisp
@@ -85,3 +85,96 @@
 (defparameter *test-account* (make-account 100 'not-secret-password))
 (funcall (funcall *test-account* 'not-secret-password 'withdraw) 40)
 (funcall (funcall *test-account* 'not-secret-password 'deposit) 50)
+
+;; *Exercise 3.4:* Modify the `make-account' procedure of *note Exercise 3-3::
+;; by adding another local state variable so that, if an account is accessed
+;; more than seven consecutive times with an incorrect password, it invokes the
+;; procedure `call-the-cops'.
+
+(defun make-account-2 (balance password)
+  (let ((password-count 0))
+    (lambda (pwd op)
+      (if (not (eq pwd password))
+          (progn (setf password-count (1+ password-count))
+                 (if (> password-count 7)
+                     (error "Call the cops")
+                     (error "Incorrect password")))
+          (progn (setf password-count 0)
+                 (let ((f (cond ((eq op 'withdraw) #'-)
+                                ((eq op 'deposit) #'+))))
+                   (lambda (x)
+                     (setf balance (funcall f balance x))
+                     balance)))))))
+
+(defparameter *test-account-2* (make-account-2 100 'not-secret-password))
+(funcall (funcall *test-account-2* 'not-secret-password 'withdraw) 50)
+
+;; *Exercise 3.5:* "Monte Carlo integration" is a method of estimating definite
+;; integrals by means of Monte Carlo simulation.  Consider computing the area of
+;; a region of space described by a predicate P(x, y) that is true for points
+;; (x, y) in the region and false for points not in the region.  For example,
+;; the region contained within a circle of radius 3 centered at (5, 7) is
+;; described by the predicate that tests whether (x - 5)^2 + (y - 7)^2 <= 3^2.
+;; To estimate the area of the region described by such a predicate, begin by
+;; choosing a rectangle that contains the region.  For example, a rectangle with
+;; diagonally opposite corners at (2, 4) and (8, 10) contains the circle above.
+;; The desired integral is the area of that portion of the rectangle that lies
+;; in the region.  We can estimate the integral by picking, at random, points
+;; (x,y) that lie in the rectangle, and testing P(x, y) for each point to
+;; determine whether the point lies in the region.  If we try this with many
+;; points, then the fraction of points that fall in the region should give an
+;; estimate of the proportion of the rectangle that lies in the region.  Hence,
+;; multiplying this fraction by the area of the entire rectangle should produce
+;; an estimate of the integral.
+
+;; Implement Monte Carlo integration as a procedure `estimate-integral' that
+;; takes as arguments a predicate `P', upper and lower bounds `x1', `x2', `y1',
+;; and `y2' for the rectangle, and the number of trials to perform in order to
+;; produce the estimate.  Your procedure should use the same `monte-carlo'
+;; procedure that was used above to estimate [pi].  Use your `estimate-integral'
+;; to produce an estimate of [pi] by measuring the area of a unit circle.
+
+;; You will find it useful to have a procedure that returns a number chosen at
+;; random from a given range.  The following `random-in-range' procedure
+;; implements this in terms of the `random' procedure used in section see 1-2-6,
+;; which returns a nonnegative number less than its input.(3)
+
+;;      (define (random-in-range low high)
+;;        (let ((range (- high low)))
+;;          (+ low (random range))))
+
+(defun random-in-range (low high)
+  (let ((random-range 100000l0)
+        (range (- high low)))
+    (+ low (* range (/ (random random-range) (1- random-range))))))
+
+(defun mote-carlo (trials experiment)
+  (labels ((iter (trials-remaining trials-passed)
+             (cond ((= trials-remaining 0)
+                    (* 1.0l0 (/ trials-passed trials)))
+                   ((funcall experiment)
+                    (iter (1- trials-remaining) (1+ trials-passed)))
+                   (t (iter (1- trials-remaining) trials-passed)))))
+    (iter trials 0)))
+
+(defun estimate-integral (pred x1 x2 y1 y2)
+    (lambda (trials)
+      (mote-carlo trials #'(lambda ()
+                             (funcall pred (random-in-range x1 x2) (random-in-range y1 y2))))))
+
+(defun mote-carlo-pi (trials)
+  (let* ((ox 0.0l0)
+         (oy 0.0l0)
+         (r 3.0l0)
+         (expt-r (expt r 2))
+         (x1 (- ox r))
+         (x2 (+ ox r))
+         (y1 (- oy r))
+         (y2 (+ oy r)))
+    (/ (* (* (- x2 x1) (- y2 y1))
+          (funcall (estimate-integral #'(lambda (x y)
+                                          (<= (+ (expt (- x ox) 2) (expt (- y oy) 2)) expt-r))
+                                      x1 x2 y1 y2) trials))
+       expt-r)))
+
+;(time (mote-carlo-pi 50000000))
